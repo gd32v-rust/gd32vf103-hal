c8b69301697b0114e6e851a530de920d6891a61a
luojia65 <me@luojia.cc>
2019-09-09 22:43:47 +0800

    Update for publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,49 @@
 //! Hardware abstract layer (HAL) for the GD32VF103 microcontroller chip.
+//! 
+//! This is an implementation of `embedded-hal` traits for the GD32VF103, chip produced by
+//! GigaDevice Semiconductor Inc. in Beijing, with one RISC-V's RV32IMAC core as well as  
+//! up to 128 KiB of Flash and 32 KiB of SRAM.
+//! 
+//! # Usage 
+//! Add this crate to your dependencies:
+//! ```toml
+//! [dependencies]
+//! stm32f1xx-hal = "0.0"
+//! ```
+//! 
+//! # Example
+//! ```Rust
+//! #![no_std]
+//! #![no_main]
+//! 
+//! extern crate panic_halt;
+//! 
+//! use riscv_rt::entry;
+//! use gd32vf103_hal as hal;
+//! use hal::prelude::*;
+//! use hal::pac as pac;
+//! 
+//! #[entry]
+//! fn main() -> ! {
+//!     // Get ownership of device peripherals
+//!     let dp = pac::Peripherals::take().unwrap();
+//!     // Constrain RCU register for further use
+//!     let mut rcu = dp.RCU.constrain();
+//!     // Split GPIOA into separate pins. 
+//!     // You need a mutable reference of APB2 struct to initialize GPIOA, 
+//!     // so we offer `&mut rcu.apb2` as a parameter here.
+//!     let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
+//!     // Change the state of `pa1` into push-pull output with default speed.
+//!     let mut pa1 = gpioa.pa1.into_push_pull_output(&mut gpioa.ctl0);
+//!     // Use API offered by `embedded-hal` to set `pa1` low,
+//!     // An LED light with cathode connected to PA1 should be lit now.
+//!     pa1.set_low().unwrap();
+//!     // We just end this program with infinite loop.
+//!     // A `wfi` instruction should be also acceptable here.
+//!     loop {}
+//! }
+//! ```
+
 #![no_std]
 #![deny(missing_docs)]
 
