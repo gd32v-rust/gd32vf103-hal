0d34619b74c149d0e4eed1bff3319729cc989731
luojia65 <me@luojia.cc>
2019-09-16 11:24:18 +0800

    Update

---
+++ examples/float.rs
@@ -0,0 +1,28 @@
+#![feature(asm, alloc_error_handler)]
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+extern crate alloc;
+
+use riscv_rt::entry;
+
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
+#[alloc_error_handler]
+fn oom(_: core::alloc::Layout) -> ! {
+    loop {}
+}
+
+use alloc::format;
+
+#[entry]
+fn main() -> ! {
+    let a = 2.33f32;
+    let s = format!("{}", a);
+    unsafe { asm!(""::"r"(s.len())) };
+    loop {}
+}
