ed8dbb1893eeb3a246467f8bef9fc69c94ced205
luojia65 <me@luojia.cc>
2019-11-23 13:20:15 +0800

    Another example

---
+++ examples/mode-after-lock-constrained.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+
+#[riscv_rt::entry]
+unsafe fn main() -> ! {
+    use gd32vf103_hal::{prelude::*, pac};
+    let dp = pac::Peripherals::take().unwrap();
+    let mut rcu = dp.RCU.constrain();
+    // Split and enable clock for GPIO port A
+    let mut gpioa = dp.GPIOA.split(&mut rcu.apb2);
+    // Switch PA0 to push-pull output with 50-MHz maximum freq
+    let pa0 = gpioa.pa0.into_push_pull_output(&mut gpioa.ctl0);
+    // Lock port A to prevent mode configurations
+    let mut pa0 = pa0.lock(&mut gpioa.lock);
+    gpioa.lock.freeze(); // drops the ownership of lock
+    // Try to switch mode for PA0 again 
+    let pa0 = pa0.into_open_drain_output(&mut gpioa.ctl0);
+    // ^ ERROR: no such method found for type `Locked<PA0<...>>`
+    loop {} 
+}
